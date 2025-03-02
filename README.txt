81f616e78bc922e7c69ecd3a2e41d437f5107b0c
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2025-03-02 21:01:06 +0900

    자기소개 웹사이트 초기 커밋

---
+++ README.txt
@@ -0,0 +1,1 @@
+https://gyeongnam.github.io/
---
+++ assets/js/swiper.js
@@ -0,0 +1,33 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper = new Swiper(".ProjectsSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
